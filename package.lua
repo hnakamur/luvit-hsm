9d2d992084ea3f95552d10445e3f49a3985d4309
Hiroaki Nakamura <hnakamur@gmail.com>
2012-07-06 01:37:54 +0900

    update version to 0.0.2

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
   name = 'hsm',
-  version = '0.0.1',
+  version = '0.0.2',
   description = 'hierarchical state machine',
   author = 'Hiroaki Nakamura <hnakamur@gmail.com>',
   main = 'hsm.lua'
